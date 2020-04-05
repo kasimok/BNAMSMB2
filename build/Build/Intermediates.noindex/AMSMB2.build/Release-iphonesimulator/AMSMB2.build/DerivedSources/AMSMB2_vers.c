dd3c384cead35164ee74d00591bbe38d0b49570d
evilisn_jiang <evilisn_jiang@trendmicro.com.cn>
2020-04-05 19:19:24 +0800

    2.7.2

---
+++ build/Build/Intermediates.noindex/AMSMB2.build/Release-iphonesimulator/AMSMB2.build/DerivedSources/AMSMB2_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char AMSMB2VersionString[];
+ extern const double AMSMB2VersionNumber;
+
+ const unsigned char AMSMB2VersionString[] __attribute__ ((used)) = "@(#)PROGRAM:AMSMB2  PROJECT:AMSMB2-1" "\n";
+ const double AMSMB2VersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/Build/Intermediates.noindex/AMSMB2.build/Release-iphonesimulator/AMSMB2.build/Objects-normal/i386/AMSMB2_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/evilisn_jiang/Desktop/AMSMB2-2.6.0/build/Build/Intermediates.noindex/AMSMB2.build/Release-iphonesimulator/AMSMB2.build/DerivedSources/AMSMB2_vers.c
